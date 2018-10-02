5853ecc522dfe44175ba35016f07030e67a6afb6
Cmeredith <tina.wheezy@gmail.com>
2018-10-02 16:50:24 -0400

    fixing a small typo in index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var express= require('express');
 var app= express();
 var port= process.env.PORT || 3000;
 
-app.us(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 app.listen(port, function(){});
